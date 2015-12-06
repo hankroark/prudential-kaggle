9cb10077927b0bb35ff8bc3ca4c5a3b95c10242a
Hank Roark <hroark@alum.mit.edu>
2015-12-06 10:48:01 -0800

    slight script editing

--- scripts/starter-script-h2o-v2.R
+++ scripts/starter-script-h2o-v2.R
@@ -1,13 +1,13 @@
 setwd("~/Documents/github/prudential-kaggle/submissions")
 
-# H2O Starter Script
+# H2O Starter Script v2
 library(h2o)
 library(readr)
 
 h2o.init(nthreads=-1)
 
 
-categoricalVariables = c("Product_Info_1", "Product_Info_2", "Product_Info_3", "Product_Info_5", "Product_Info_6", "Product_Info_7", 
+categoricalVariables <- c("Product_Info_1", "Product_Info_2", "Product_Info_3", "Product_Info_5", "Product_Info_6", "Product_Info_7", 
                          "Employment_Info_2", "Employment_Info_3", "Employment_Info_5", 
                          "InsuredInfo_1", "InsuredInfo_2", "InsuredInfo_3", "InsuredInfo_4", "InsuredInfo_5", "InsuredInfo_6", "InsuredInfo_7", 
                          "Insurance_History_1", "Insurance_History_2", "Insurance_History_3", "Insurance_History_4", "Insurance_History_7", 
@@ -20,8 +20,8 @@ categoricalVariables = c("Product_Info_1", "Product_Info_2", "Product_Info_3", "
                          "Medical_History_25", "Medical_History_26", "Medical_History_27", "Medical_History_28", "Medical_History_29", 
                          "Medical_History_30", "Medical_History_31", "Medical_History_33", "Medical_History_34", "Medical_History_35", 
                          "Medical_History_36", "Medical_History_37", "Medical_History_38", "Medical_History_39", "Medical_History_40", "Medical_History_41")
-specifiedTypes = rep("Enum", length(categoricalVariables))
-col.types=list(by.col.name=categoricalVariables,types=specifiedTypes)
+specifiedTypes <- rep("Enum", length(categoricalVariables))
+col.types <- list(by.col.name=categoricalVariables,types=specifiedTypes)
 
 cat("reading the train and test data\n")
 train <- h2o.uploadFile("../input/train.csv", col.types=col.types)
